1fc488e95c5e81e3a3e98c9c05173e25ce07ab12
Akash <akashwarrior1@gmail.com>
2024-10-29 01:43:07 +0530

    Update page.tsx

--- frontend/src/app/upload/page.tsx
+++ frontend/src/app/upload/page.tsx
@@ -2,7 +2,7 @@ import { Upload } from "@/components/Upload";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-export default async function () {
+export default async function uploadPage() {
   const session = await getServerSession();
   if (!session) {
     redirect("/");
